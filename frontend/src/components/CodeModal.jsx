b0ba329a77408af5cd5d8f3e23097afd70ae3c56
Ayende Rahien <ayende@ayende.com>
2024-02-02 10:52:08 +0200

    Handling ESC to abort code view

--- frontend/src/components/CodeModal.jsx
+++ frontend/src/components/CodeModal.jsx
@@ -3,13 +3,24 @@ import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 import { dracula } from "react-syntax-highlighter/dist/esm/styles/prism";
 import "../styles/components/code-modal.css";
 
-
-
-
 function CodeModal({ code, onClose }) {
+
+  function onKeyDown(e) {
+    if (e.key == "Escape") {
+      closeModal();
+    }
+  }
+
+  document.addEventListener("keydown", onKeyDown, false);
+
+  function closeModal() {
+    document.removeEventListener("keydown", onKeyDown, false);
+    onClose();
+  }
+
   return (
     <>
-      <div className="code-modal-main-screen" onClick={onClose}></div>
+      <div className="code-modal-main-screen" onClick={onClose} onKeyDownCapture={onKeyDown}></div>
       <div className="code-modal">
         <h3 className="code-modal-title">Reading the code!</h3>
         <SyntaxHighlighter language="javascript" style={dracula}>{code}</SyntaxHighlighter>
